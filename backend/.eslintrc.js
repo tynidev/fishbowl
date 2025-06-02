af62396ca1af7b519fb2c2824e32dc6d4e311ada
tynidev <nichols.tyler@gmail.com>
2025-06-01 22:04:34 -0600

    fix: disable 'no-explicit-any' rule in ESLint configuration

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -26,7 +26,7 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'warn',
     
     // General rules
--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -47,7 +47,7 @@ module.exports = [
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
       'no-unused-vars': 'off',
       'prefer-const': 'error',
       'no-var': 'error',
